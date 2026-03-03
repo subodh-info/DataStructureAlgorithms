a7c2fd255d9f37240617b57b5a3a2d912361b491
subodh-info <subodhkr0692@gmail.com>
2026-03-03 11:02:56 +0530

    Verify the number is divisible by 15

---
+++ Maths/divisible15.cpp
@@ -0,0 +1,13 @@
+/*
+A program to check if a number is divisible by 15 or not.
+To check if a number is divisible by 15, we can use the following method:
+1. Check if the number is divisible by 3.
+2. Check if the number is divisible by 5.
+If both conditions are satisfied, then the number is divisible by 15.
+
+We have already implemented the logic to check if a number is divisible by 3 and 5 in the previous programs (DivisibleBy3.cpp and DivisibleBy5.cpp).
+Hence, here I'm not going to implement logic for checking divisibility by 15 because,
+if a number is divisible by 15, it must be divisible by both 3 and 5.
+So, we can simply check if the number is divisible by 3 and 5 using the functions we have already implemented in the previous programs.
+
+*/
