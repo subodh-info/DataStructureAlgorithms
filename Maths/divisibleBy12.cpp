0b64cc2d470d8cfc1c4df889d2c262a0bcf2ce26
subodh-info <subodhkr0692@gmail.com>
2026-03-03 10:25:01 +0530

    Verify number is divisible by 12

---
+++ Maths/divisibleBy12.cpp
@@ -0,0 +1,15 @@
+/* A Program to check if a number is divisible by 12 or not.
+To check if a number is divisible by 12, 
+
+There are two conditions that must be satisfied:
+1. The number must be divisible by 3.
+2. The number must be divisible by 4.
+
+We have already implemented the logic to check if a number is divisible by 3 and 4 in the previous programs (DivisibleBy3.cpp and DivisibleBy4.cpp).
+
+Hence, here I'm not going to implement logic for checking divisibility by 12 because, 
+if a number is divisible by 12, it must be divisible by both 3 and 4.
+So, we can simply check if the number is divisible by 3 and 4 using 
+the functions we have already implemented in the previous programs.
+
+*/
