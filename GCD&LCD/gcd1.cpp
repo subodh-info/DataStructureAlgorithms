42c83fbfad7f21125bc2f789f5120bc258c3c171
subodh-info <subodhkr0692@gmail.com>
2026-03-03 12:52:58 +0530

    calculate GCD using STL's function std::gcd()

--- GCD&LCD/gcd1.cpp
+++ GCD&LCD/gcd1.cpp
@@ -1,8 +1,7 @@
 /* A program to calculate the GCD of two integer. */
 
 #include <iostream>
-#include <algorithm>
-
+#include <numeric>
 int gcd(int a, int b) {
     while(b != 0) {
         int remainder = a % b;
@@ -19,11 +18,21 @@ int gcd_recursive(int a, int b) {
     return gcd_recursive(b, a % b);
 }
 
+
+// calculate gcd using STL
+int gcd_stl(int a, int b) {
+    return std::gcd(a, b); // Here, std::gcd is a function from the <numeric> header 
+    // that computes the greatest common divisor of two integers.
+    // It is available in C++17 and later versions. 
+    // Internally it uses the Euclidean algorithm, which is efficient for computing the GCD.
+}
 int main(){
     int a = 18, b = 48;
     std::cout << "The GCD of " << a << " and " << b << " is: " << gcd(a, b) << std::endl;
 
     std::cout << "The GCD of " << a << " and " << b << " using recursion is: " << gcd_recursive(a, b) << std::endl;
+
+    std::cout << "The GCD of " << a << " and " << b << " using STL is: " << gcd_stl(a, b) << std::endl;
 }
 
 // NOTE:- gcd(a, b) = gcd(b, a);
