9b878ddff1882e1c2cfe3bd8be241d570bfe0ebf
subodh-info <subodhkr0692@gmail.com>
2026-03-03 12:47:38 +0530

    Calculate GCD using recursion

--- GCD&LCD/gcd1.cpp
+++ GCD&LCD/gcd1.cpp
@@ -12,9 +12,18 @@ int gcd(int a, int b) {
     return a;
 }
 
+// calculate gcd using recursion
+int gcd_recursive(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd_recursive(b, a % b);
+}
+
 int main(){
     int a = 18, b = 48;
     std::cout << "The GCD of " << a << " and " << b << " is: " << gcd(a, b) << std::endl;
+
+    std::cout << "The GCD of " << a << " and " << b << " using recursion is: " << gcd_recursive(a, b) << std::endl;
 }
 
 // NOTE:- gcd(a, b) = gcd(b, a);
