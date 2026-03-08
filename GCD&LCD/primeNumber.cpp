ab5de2da5611cd6b38082c3014f03f4e5e7b364b
subodh-info <subodhkr0692@gmail.com>
2026-03-08 15:50:55 +0530

    Optimize program to check prime number

--- GCD&LCD/primeNumber.cpp
+++ GCD&LCD/primeNumber.cpp
@@ -7,12 +7,38 @@ positive divisors: 1 and themselves.
 #include <iostream>
 #include <cmath>
 
+/*
 bool isPrime(int num) {
     if(num < 0) {
         return false;
     }
     bool status = true;
     for(int i = 2; i<= std::sqrt(num); i++) {
+        if(num % i == 0) {
+            status = false;
+            break;
+        }
+    }
+    return status;
+}
+*/
+/*
+Time complexity of this program is : O(sqrt(n))
+*/
+
+// Another approach with time complexity: O(sqrt(n)/2)
+
+bool isPrime(int num) {
+    if(num < 0) {
+        return false;
+    }
+    if(num == 2) {
+        return true;
+    }
+
+    bool status = true;
+    // here skiping the even number. Because, except 2 other even number can not be prime number.
+    for(int i = 3; i<= std::sqrt(num); i+=2) {
         if(num % i == 0) {
             status = false;
             break;
