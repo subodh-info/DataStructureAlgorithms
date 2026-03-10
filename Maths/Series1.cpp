18522fb361e2f5c9dc9647bbfca8cf3cec9508e9
subodh-info <subodhkr0692@gmail.com>
2026-03-10 08:12:27 +0530

    Calculate sum of series

---
+++ Maths/Series1.cpp
@@ -0,0 +1,44 @@
+/* A program to caluclate the sume of below patterns - 
+    sum = 2 + 22 = 24
+    sum = 2 + 22 + 222 = 246
+Here, we are writing the program for this pattern - 
+    2+22+222+2222+............N terms
+*/
+
+#include <iostream>
+#include <cmath>
+long sum(int N) {
+    long _sum = 0;
+    int terms = 0;
+    for(int i = 1; i <= N; i++) {
+        terms = terms*10 + 2;
+        _sum = _sum + terms;
+    }
+
+    return _sum;
+}
+
+// Using recursion
+long gSum = 0;
+int gTerm = 0;
+long sumRec(int N) {
+    if(N == 0) {
+        return 0;
+    }
+    gTerm = gTerm * 10 + 2;
+    gSum += gTerm;
+    sumRec(N-1);
+    return gSum;
+}
+
+// Using mathemtaical formulla
+long sumMath(int N) {
+    return (2.0/81) * (10 * (pow(10, N) - 1) - 9 * N);
+}
+
+int main(){
+    int N = 4;
+    // std::cout << sum(N) << std::endl;
+    // std::cout << sumRec(N) << std::endl;
+    std::cout << sumMath(N) << std::endl;
+}
